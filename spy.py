b032538a7c367a77219376663422817f8b3a238a
NurTasin <nmuatasin2005@gmail.com>
2021-11-19 12:33:07 +0600

    Bug Fix of not downloading the comments

--- spy.py
+++ spy.py
@@ -3,20 +3,20 @@ from sys import argv
 import json
 import time
 
-
+cycle=0
 while True:
+    cycle+=1
     startingTime=time.time()
     with open("./conf.json") as handle:
         data=json.load(handle)
     options=""
     if data["update"]:
         options+=" -F "
-    system("instaloader "+options+" --dirname-pattern='./data/{target}' -l "+data["login-data"]["username"]+" ".join(data["targets"]))
-    options=""
     if data["fetch-comments"]:
-        options+=" --no-videos --no-video-thumbnails --no-pictures --comments "
-        system("instaloader "+options+" --dirname-pattern='./data/{target}' -l "+data["login-data"]["username"]+" ".join(data["targets"]))
+        options+=" --comments "
+    system("instaloader "+options+" --dirname-pattern=\"./data/{target}\" -l "+data["login-data"]["username"]+" "+" ".join(data["targets"]))
     endingTime=time.time()
-    if (endingTime-startingTime) > 0:
+    if data["delay"]-(endingTime-startingTime) > 0:
+        print(f"Cycle {cycle} ended and waiting {data['delay']-(endingTime-startingTime)} s before starting the next cycle.")
         time.sleep(data["delay"]-(endingTime-startingTime))
 
